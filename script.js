1776a5011e7f7ea0dba69f8c43d6957f8a80ed1a
tenaz21 <tenazoa72922787@gmail.com>
2026-04-13 02:58:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,85 @@
 const API_KEY = "46146fa668msh7fabe31efaaf8dep198920jsn3151b4847140";
 
-fetch("https://football-api-7.p.rapidapi.com/api/matches/live",{
+async function cargarEnVivo(){
+
+const contenido = document.getElementById("contenido");
+
+contenido.innerHTML = "Cargando partidos...";
+
+try{
+
+let response = await fetch(
+"https://football-api-7.p.rapidapi.com/api/matches/live",
+{
 headers:{
 "X-RapidAPI-Key":API_KEY,
 "X-RapidAPI-Host":"football-api-7.p.rapidapi.com"
 }
-})
-.then(res=>{
-console.log(res);
-return res.json();
-})
-.then(data=>{
-console.log(data);
-})
-.catch(err=>{
-console.log(err);
 });
+
+let data = await response.json();
+
+if(data.events && data.events.length > 0){
+
+mostrarPartidos(data.events);
+
+}else{
+
+response = await fetch(
+"https://football-api-7.p.rapidapi.com/api/matches/top",
+{
+headers:{
+"X-RapidAPI-Key":API_KEY,
+"X-RapidAPI-Host":"football-api-7.p.rapidapi.com"
+}
+});
+
+data = await response.json();
+
+if(data.events && data.events.length > 0){
+
+mostrarPartidos(data.events);
+
+}else{
+
+contenido.innerHTML="<h2>No hay partidos disponibles.</h2>";
+
+}
+
+}
+
+}catch(error){
+
+contenido.innerHTML="<h2>Error cargando API.</h2>";
+
+console.log(error);
+
+}
+
+}
+
+function mostrarPartidos(partidos){
+
+const contenido = document.getElementById("contenido");
+
+contenido.innerHTML="";
+
+partidos.forEach(match=>{
+
+contenido.innerHTML += `
+<div class="card">
+<h2>${match.homeTeam.name} vs ${match.awayTeam.name}</h2>
+
+<div class="score">
+${match.homeScore?.current ?? 0} - ${match.awayScore?.current ?? 0}
+</div>
+
+<p>${match.tournament?.name ?? "Liga"}</p>
+</div>
+`;
+
+});
+
+}
+
+window.onload = cargarEnVivo;
